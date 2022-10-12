bc607781249e58c9c97b1eb14db04de69a2052d2
switch-blade-stuff <terekhin.mikhail.01@gmail.com>
2022-10-11 18:51:24 -0700

    Forgot to remove API macro example

--- sekhmet/detail/platform.h
+++ sekhmet/detail/platform.h
@@ -48,31 +48,6 @@
 #define SEK_FILE __FILE__
 #define SEK_LINE __LINE__
 
-#if defined _WIN32
-#ifdef BUILDING_DLL
-#ifdef __GNUC__
-#define DLL_PUBLIC __attribute__((dllexport))
-#else
-#define DLL_PUBLIC __declspec(dllexport)	// Note: actually gcc seems to also supports this syntax.
-#endif
-#else
-#ifdef __GNUC__
-#define DLL_PUBLIC __attribute__((dllimport))
-#else
-#define DLL_PUBLIC __declspec(dllimport)	// Note: actually gcc seems to also supports this syntax.
-#endif
-#endif
-#define DLL_LOCAL
-#else
-#if __GNUC__ >= 4
-#define DLL_PUBLIC __attribute__((visibility("default")))
-#define DLL_LOCAL __attribute__((visibility("hidden")))
-#else
-#define DLL_PUBLIC
-#define DLL_LOCAL
-#endif
-#endif
-
 #ifdef SEK_OS_WIN
 
 #define SEK_API_HIDDEN
