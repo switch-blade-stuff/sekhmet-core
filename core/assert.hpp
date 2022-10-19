6c8765a38ad849f48892bc9561517afb82801505
switch-blade-stuff <terekhin.mikhail.01@gmail.com>
2022-10-19 03:57:55 -0700

    Added debug trap support

--- core/assert.hpp
+++ core/assert.hpp
@@ -10,6 +10,8 @@
 
 namespace sek::detail
 {
+	SEK_CORE_PUBLIC void debug_trap() noexcept;
+
 #ifndef __cpp_lib_unreachable
 	[[noreturn]] SEK_CORE_PUBLIC void assert_unreachable(const char *file, std::size_t line, const char *func);
 #endif
@@ -21,7 +23,10 @@ namespace sek::detail
 		if (!cond) [[unlikely]]
 		{
 			if (!std::is_constant_evaluated())
+			{
+				debug_trap();
 				assert_fail(file, line, func, cond_str, msg);
+			}
 			else
 				constexpr_assert_fail(msg);
 		}
@@ -33,7 +38,7 @@ namespace sek::detail
 
 /** Same as regular SEK_ASSERT, except applies even when SEK_NO_DEBUG_ASSERT is defined. */
 #define SEK_ASSERT_ALWAYS(...) SEK_GET_MACRO_2(__VA_ARGS__, SEK_ASSERT_2, SEK_ASSERT_1)(__VA_ARGS__)
-
+#define SEK_DEBUG_TRAP_ALWAYS() sek::detail::debug_trap()
 /** Asserts that the code should never be reached. */
 #ifndef __cpp_lib_unreachable
 #define SEK_NEVER_REACHED sek::detail::assert_unreachable((SEK_FILE), (SEK_LINE), (SEK_PRETTY_FUNC))
@@ -45,6 +50,8 @@ namespace sek::detail
 /** Assert that supports an optional message, prints the enclosing function name and terminates using exit(1).
  * @note Currently the message can only be a char literal. */
 #define SEK_ASSERT(...) SEK_ASSERT_ALWAYS(__VA_ARGS__)
+#define SEK_DEBUG_TRAP() SEK_DEBUG_TRAP_ALWAYS()
 #else
 #define SEK_ASSERT(...)
+#define SEK_DEBUG_TRAP()
 #endif
--- core/detail/assert.cpp
+++ core/detail/assert.cpp
@@ -7,8 +7,38 @@
 #include <cstdio>
 #include <cstdlib>
 
+#if defined(__has_builtin) && !defined(__ibmxl__)
+#if __has_builtin(__builtin_debugtrap)
+#define DEBUG_TRAP() __builtin_debugtrap()
+#elif __has_builtin(__debugbreak)
+#define DEBUG_TRAP() __debugbreak()
+#endif
+#elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#define DEBUG_TRAP() __debugbreak()
+#elif defined(__ARMCC_VERSION)
+#define DEBUG_TRAP() __breakpoint(42)
+#elif defined(__ibmxl__) || defined(__xlC__)
+#include <builtins.h>
+#define DEBUG_TRAP() __trap(42)
+#elif defined(__DMC__) && defined(_M_IX86)
+#define DEBUG_TRAP() __asm int 3h
+#elif defined(__i386__) || defined(__x86_64__)
+#define DEBUG_TRAP __asm__ __volatile__("int3")
+#elif defined(__STDC_HOSTED__) && (__STDC_HOSTED__ == 0) && defined(__GNUC__)
+#define DEBUG_TRAP() __builtin_trap()
+#else
+#include <signal.h>
+#if defined(SIGTRAP)
+#define DEBUG_TRAP() raise(SIGTRAP)
+#else
+#define DEBUG_TRAP() raise(SIGABRT)
+#endif
+#endif
+
 namespace sek::detail
 {
+	void debug_trap() noexcept { DEBUG_TRAP(); }
+
 	void assert_unreachable(const char *file, std::size_t line, const char *func)
 	{
 		fprintf(stderr, "Reached unreachable code at '%s:%lu' in '%s'\n", file, line, func);
