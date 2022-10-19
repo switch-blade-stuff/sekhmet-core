590dd822525ae8b58b23d5f586efb309874c019c
switch-blade-stuff <terekhin.mikhail.01@gmail.com>
2022-10-19 01:48:24 -0700

    Fixed potential iterator invalidation on insert for buffered_vector

--- core/buffered_vector.hpp
+++ core/buffered_vector.hpp
@@ -59,10 +59,12 @@ namespace sek
 		{
 			for (size_type i = n; i-- != 0;) relocate(src + i, dst + i);
 		}
+
 		constexpr static void relocate_n(T *src, size_type n, T *dst)
 		{
 			for (size_type i = 0; i < n; ++i) relocate(src + i, dst + i);
 		}
+
 		constexpr static void relocate(T *src, T *dst)
 		{
 			std::construct_at(dst, std::move(*src));
@@ -71,6 +73,7 @@ namespace sek
 
 	public:
 		constexpr buffered_vector() noexcept(detail::nothrow_alloc_construct<Alloc>) : m_size(), m_local() {}
+
 		constexpr ~buffered_vector()
 		{
 			if (local())
@@ -87,17 +90,20 @@ namespace sek
 			: alloc_base(alloc)
 		{
 		}
+
 		/** Initializes the vector filled with `n` default-constructed elements using the provided allocator instance. */
 		constexpr buffered_vector(size_type n, const Alloc &alloc = Alloc{}) : buffered_vector(alloc)
 		{
 			insert(end(), n);
 		}
+
 		/** Initializes the vector filled with `n` copies of `value` using the provided allocator instance. */
 		constexpr buffered_vector(size_type n, const value_type &value, const Alloc &alloc = Alloc{})
 			: buffered_vector(alloc)
 		{
 			insert(end(), n, value);
 		}
+
 		/** Initializes the vector filled with elements from range `[first, last)` using the provided allocator instance.
 		 * @param first Iterator to the start of the source range.
 		 * @param last Sentinel of `first`.
@@ -107,6 +113,7 @@ namespace sek
 		{
 			insert(end(), first, last);
 		}
+
 		/** Initializes the vector filled with elements from an initializer list using the provided allocator instance.
 		 * @param il Initializer list containing vector elements.
 		 * @param alloc Allocator used to allocate memory of the vector. */
@@ -120,6 +127,7 @@ namespace sek
 		{
 			insert(end(), other.begin(), other.end());
 		}
+
 		/** Copy-constructs the vector using the provided allocator instance. */
 		constexpr buffered_vector(const buffered_vector &other, const Alloc &alloc) : buffered_vector(alloc)
 		{
@@ -134,6 +142,7 @@ namespace sek
 			else
 				move_data(other);
 		}
+
 		/** Move-constructs the vector using the provided allocator instance. */
 		constexpr buffered_vector(buffered_vector &&other, const Alloc &alloc) : buffered_vector(alloc)
 		{
@@ -173,6 +182,7 @@ namespace sek
 			}
 			return *this;
 		}
+
 		constexpr buffered_vector &operator=(buffered_vector &&other) noexcept(detail::nothrow_alloc_move_assign<Alloc>)
 		{
 			if constexpr (alloc_traits::propagate_on_container_move_assignment::value)
@@ -199,6 +209,7 @@ namespace sek
 			clear();
 			insert(end(), n, value);
 		}
+
 		/** Replaces elements of the vector with contents of a range `[first, last)`.
 		 * @param first Iterator to the start of the source range.
 		 * @param last Sentinel of `first`. */
@@ -208,6 +219,7 @@ namespace sek
 			clear();
 			insert(end(), first, last);
 		}
+
 		/** Replaces elements of the vector with contents of an initializer list.
 		 * @param il Initializer list containing new contents of the vector. */
 		constexpr void assign(std::initializer_list<T> il)
@@ -215,6 +227,7 @@ namespace sek
 			clear();
 			insert(end(), il);
 		}
+
 		/** @copydoc assign */
 		constexpr buffered_vector &operator=(std::initializer_list<T> il)
 		{
@@ -224,54 +237,73 @@ namespace sek
 
 		/** Returns iterator to the first element of the vector. */
 		[[nodiscard]] constexpr iterator begin() noexcept { return iterator{data()}; }
+
 		/** @copydoc begin */
 		[[nodiscard]] constexpr const_iterator begin() const noexcept { return const_iterator{data()}; }
+
 		/** @copydoc begin */
 		[[nodiscard]] constexpr const_iterator cbegin() const noexcept { return begin(); }
+
 		/** Returns iterator one past the last element of the vector. */
 		[[nodiscard]] constexpr iterator end() noexcept { return iterator{data() + size()}; }
+
 		/** @copydoc end */
 		[[nodiscard]] constexpr const_iterator end() const noexcept { return const_iterator{data() + size()}; }
+
 		/** @copydoc end */
 		[[nodiscard]] constexpr const_iterator cend() const noexcept { return end(); }
 
 		/** Returns reverse iterator to the last element of the vector. */
 		[[nodiscard]] constexpr reverse_iterator rbegin() noexcept { return reverse_iterator{end()}; }
+
 		/** @copydoc rbegin */
 		[[nodiscard]] constexpr const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator{end()}; }
+
 		/** @copydoc rbegin */
 		[[nodiscard]] constexpr const_reverse_iterator crbegin() const noexcept { return rbegin(); }
+
 		/** Returns reverse iterator one past the firt element of the vector. */
 		[[nodiscard]] constexpr reverse_iterator rend() noexcept { return reverse_iterator{begin()}; }
+
 		/** @copydoc rend */
 		[[nodiscard]] constexpr const_reverse_iterator rend() const noexcept { return const_reverse_iterator{begin()}; }
+
 		/** @copydoc rend */
 		[[nodiscard]] constexpr const_reverse_iterator crend() const noexcept { return rend(); }
 
 		/** Returns the current size of the vector. */
 		[[nodiscard]] constexpr size_type size() const noexcept { return m_size.value(); }
+
 		/** Returns the maximum possible size of the vector. */
 		[[nodiscard]] constexpr size_type max_size() const noexcept { return std::numeric_limits<size_type>() / 2; }
+
 		/** Returns the current capacity of the vector. */
 		[[nodiscard]] constexpr size_type capacity() const noexcept { return local() ? N : m_heap.capacity; }
+
 		/** Checks if the vector is empty (`size() == 0`). */
 		[[nodiscard]] constexpr bool empty() const noexcept { return size() == 0; }
+
 		/** Checks if the vector is stored locally (capacity() <= N). */
 		[[nodiscard]] constexpr bool local() const noexcept { return !m_size.flag(); }
 
 		/** Returns reference to the first element of the vector. */
 		[[nodiscard]] constexpr reference front() noexcept { return *begin(); }
+
 		/** @copydoc front */
 		[[nodiscard]] constexpr const_reference front() const noexcept { return *begin(); }
+
 		/** Returns reference to the last element of the vector. */
 		[[nodiscard]] constexpr reference back() noexcept { return std::prev(*end()); }
+
 		/** @copydoc back */
 		[[nodiscard]] constexpr const_reference back() const noexcept { return std::prev(*end()); }
 
 		/** Returns reference to the element located at offset `i`. */
 		[[nodiscard]] constexpr reference operator[](size_type i) noexcept { return data()[i]; }
+
 		/** @copydoc operator[] */
 		[[nodiscard]] constexpr const_reference operator[](size_type i) const noexcept { return data()[i]; }
+
 		/** @copydoc operator[]
 		 * @throw std::out_of_range If `i` is out of range. */
 		[[nodiscard]] constexpr reference at(size_type i)
@@ -279,6 +311,7 @@ namespace sek
 			check_subscript(i);
 			return operator[](i);
 		}
+
 		/** @copydoc at */
 		[[nodiscard]] constexpr const_reference at(size_type i) const
 		{
@@ -288,6 +321,7 @@ namespace sek
 
 		/** Returns pointer to the internal data buffer. */
 		[[nodiscard]] constexpr pointer data() noexcept { return local() ? m_local.template get<T>() : m_heap.data; }
+
 		/** Returns pointer to the internal data buffer. */
 		[[nodiscard]] constexpr const_pointer data() const noexcept
 		{
@@ -300,6 +334,7 @@ namespace sek
 			std::destroy_n(data(), size());
 			m_size.value(0);
 		}
+
 		/** Shrinks the internal buffer to fit it's size. If the size is less than the local buffer limit,
 		 * moves the vector data to local buffer. */
 		constexpr void shrink_to_fit()
@@ -360,6 +395,7 @@ namespace sek
 		/** Resizes the vector to `n` elements. Any newly-constructed elements are default-constructed.
 		 * @throw std::length_error If the requested size exceeds `max_size`. */
 		constexpr void resize(size_type n) { resize_impl(n); }
+
 		/** Resizes the vector to `n` elements. Any newly-constructed elements are copied from `value`.
 		 * @throw std::length_error If the requested size exceeds `max_size`. */
 		constexpr void resize(size_type n, const value_type &value) { resize_impl(n, value); }
@@ -372,20 +408,9 @@ namespace sek
 		template<typename... Args>
 		constexpr iterator emplace(const_iterator where, Args &&...args)
 		{
-			const auto back_emplace = where == end();
-			const auto result = make_space(where, 1);
-			try
-			{
-				std::construct_at(result.get(), std::forward<Args>(args)...);
-			}
-			catch (...)
-			{
-				/* Follow the strong exception guarantee of `std::vector` and rollback the size. */
-				if (back_emplace) m_size.value(m_size.size() - 1);
-				throw;
-			}
-			return result;
+			return emplace_impl(where, 1, [&](T *ptr) { std::construct_at(ptr, std::forward<Args>(args)...); })
 		}
+
 		/** Inserts an in-place constructed element at the end of the vector.
 		 * @param args Arguments passed to the element's constructor.
 		 * @return Reference to the inserted element.
@@ -402,17 +427,20 @@ namespace sek
 		 * @return Iterator to the inserted element.
 		 * @note Follows the exception guarantee of `std::vector`. */
 		constexpr iterator insert(const_iterator where, const value_type &value) { return emplace(where, value); }
+
 		/** Inserts a move-constructed element at the specified position.
 		 * @param where Position within the vector at which to insert the element.
 		 * @param value Value to insert.
 		 * @return Iterator to the inserted element.
 		 * @note Follows the exception guarantee of `std::vector`. */
 		constexpr iterator insert(const_iterator where, value_type &&value) { return emplace(where, std::move(value)); }
+
 		/** Inserts a copy-constructed element at the end of the vector.
 		 * @param value Value to insert.
 		 * @return Reference to the inserted element.
 		 * @note Follows the exception guarantee of `std::vector`. */
 		constexpr reference push_back(const value_type &value) { return emplace_back(value); }
+
 		/** Inserts a move-constructed element at the end of the vector.
 		 * @param value Value to insert.
 		 * @return Reference to the inserted element.
@@ -427,21 +455,9 @@ namespace sek
 		 * @note Follows the exception guarantee of `std::vector`. */
 		constexpr iterator insert(const_iterator where, size_type n, const value_type &value)
 		{
-			const auto back_emplace = where == end();
-			const auto result = make_space(where, n);
-			size_type i = 0;
-			try
-			{
-				while (i < n) std::construct_at(result.get() + (i++), value);
-			}
-			catch (...)
-			{
-				/* Follow the strong exception guarantee of `std::vector` and rollback the size. */
-				if (back_emplace && i == n) m_size.value(m_size.size() - 1);
-				throw;
-			}
-			return result;
+			return emplace_impl(where, n, [&](T *ptr) { std::construct_at(ptr, value); });
 		}
+
 		/** Inserts elements from range `[first, last)` at the specified position.
 		 * @param where Position within the vector at which to insert the elements.
 		 * @param first Iterator to the start of the source range.
@@ -465,25 +481,17 @@ namespace sek
 			}
 			return result;
 		}
+
 		/** @copydoc insert */
 		template<std::random_access_iterator I, std::sentinel_for<I> S>
 		constexpr iterator insert(const_iterator where, I first, S last)
 		{
-			const auto n = static_cast<size_type>(std::distance(first, last));
-			const auto back_emplace = where == end();
-			const auto result = make_space(where, n);
-			try
-			{
-				for (auto dst = result.get(); first < last; ++dst) std::construct_at(dst, *(first++));
-			}
-			catch (...)
-			{
-				/* Follow the strong exception guarantee of `std::vector` and rollback the size. */
-				if (back_emplace && first == last) m_size.value(m_size.size() - 1);
-				throw;
-			}
-			return result;
+			// clang-format off
+			return emplace_impl(where,static_cast<size_type>(std::distance(first, last)),
+								[&](T *ptr) { std::construct_at(ptr, *first++); });
+			// clang-format on
 		}
+
 		/** Inserts elements from the initializer list at the specified position.
 		 * @param where Position within the vector at which to insert the elements.
 		 * @param il Initializer list containing elements to insert.
@@ -498,6 +506,7 @@ namespace sek
 		 * @param where Position of the element to erase.
 		 * @return Iterator to the element after the erased one or `end()`. */
 		constexpr iterator erase(const_iterator where) { return erase(where, where + 1); }
+
 		/** Removes a range of elements `[first, last)` from the vector.
 		 * @param first Iterator to the start of the erased sequence.
 		 * @param last Iterator to the end of the erased sequence.
@@ -521,6 +530,7 @@ namespace sek
 			m_size.value(m_size.value() - amount);
 			return dst;
 		}
+
 		/** Removes the last element from the vector. */
 		constexpr void pop_back()
 		{
@@ -535,6 +545,7 @@ namespace sek
 		{
 			return std::lexicographical_compare_three_way(begin(), end(), other.begin(), other.end());
 		}
+
 		[[nodiscard]] constexpr bool operator==(const buffered_vector &other) const noexcept
 			requires(requires(const_iterator a, const_iterator b) { std::equal_to<>{}(*a, *b); })
 		{
@@ -542,13 +553,14 @@ namespace sek
 		}
 
 		// clang-format off
-		constexpr void swap(buffered_vector &other) noexcept requires std::is_swappable_v<T>
-		{
-			alloc_assert_swap(alloc(), other.alloc());
-			alloc_swap(alloc(), other.alloc());
-			swap_data(other);
-		}
-		friend constexpr void swap(buffered_vector &a, buffered_vector &b) noexcept requires std::is_swappable_v<T> { a.swap(b); }
+        constexpr void swap(buffered_vector &other) noexcept requires std::is_swappable_v<T> {
+            alloc_assert_swap(alloc(), other.alloc());
+            alloc_swap(alloc(), other.alloc());
+            swap_data(other);
+        }
+
+        friend constexpr void
+        swap(buffered_vector &a, buffered_vector &b) noexcept requires std::is_swappable_v<T> { a.swap(b); }
 		// clang-format on
 
 	private:
@@ -559,6 +571,7 @@ namespace sek
 		}
 
 		[[nodiscard]] constexpr auto &alloc() noexcept { return *alloc_base::get(); }
+
 		[[nodiscard]] constexpr auto &alloc() const noexcept { return *alloc_base::get(); }
 
 		constexpr void swap_data(buffered_vector &other) noexcept
@@ -685,6 +698,7 @@ namespace sek
 				std::swap(m_heap, other.m_heap);
 			}
 		}
+
 		constexpr void move_data(buffered_vector &other) noexcept
 		{
 			const auto other_size = other.size();
@@ -703,6 +717,46 @@ namespace sek
 			else
 				for (; dst != this_end; ++dst) std::destroy_at(dst.get());
 			m_size.value(other_size);
+		}
+
+		template<typename F, typename... Args>
+		constexpr iterator emplace_impl(const_iterator where, size_type n, F &&factory)
+		{
+			const auto insert_pos = static_cast<size_type>(where - cbegin());
+			const auto old_size = size();
+			const auto new_size = old_size + n;
+			const auto old_cap = capacity();
+			const auto resized = old_cap <= new_size;
+			const auto new_cap = std::max(old_cap * 2, new_size);
+
+			const auto src = data();
+			const auto dst = resized ? alloc_traits::allocate(alloc(), new_cap) : src;
+
+			/* Move everything that is above `insert_pos` */
+			relocate_backwards_n(src + insert_pos, old_size - insert_pos, dst + insert_pos + n);
+
+			try
+			{
+				for (size_type i = 0; i < n; ++i) factory(dst + insert_pos + i);
+			}
+			catch (...)
+			{
+				/* If a new buffer was allocated, deallocate it. */
+				if (resized) alloc_traits::deallocate(alloc(), dst, new_cap);
+				throw;
+			}
+
+			/* If resizing was necessary, move old data. */
+			if (resized)
+			{
+				relocate_n(src, insert_pos, dst);
+				if (!local()) alloc_traits::deallocate(alloc(), src, old_cap);
+				m_heap.capacity = new_cap;
+				m_heap.data = dst;
+				m_size.flag(true);
+			}
+
+			return begin() + static_cast<difference_type>(insert_pos);
 		}
 
 		constexpr iterator make_space(const_iterator where, size_type n)
@@ -763,6 +817,7 @@ namespace sek
 		v.erase(i, v.end());
 		return static_cast<typename buffered_vector<T, N, Alloc>::size_type>(d);
 	}
+
 	/** Erases all elements that satisfy the predicate `pred` from the vector. */
 	template<typename T, std::size_t N, typename Alloc, typename P>
 	constexpr typename buffered_vector<T, N, Alloc>::size_type erase_if(buffered_vector<T, N, Alloc> &v, P &&pred)
